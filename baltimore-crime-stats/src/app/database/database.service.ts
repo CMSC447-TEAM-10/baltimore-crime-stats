bbd13ce7ffb1a2d861abb4cb0b9d107d72a2b46a
Juno Park <GothLoli@Junos-MBP.fios-router.home>
2020-05-11 06:05:43 -0400

    fix bug

---
+++ baltimore-crime-stats/src/app/database/database.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatabaseService {
+  constructor(private db: AngularFireDatabase) { }
+
+  private _get
+}
