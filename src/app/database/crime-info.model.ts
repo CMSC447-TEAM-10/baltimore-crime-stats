29a598db980b3418b1b906867f952f5bca767339
Corey Scheideman <coreys1@umbc.edu>
2020-05-11 21:17:07 -0400

    fill out database service and model

--- src/app/database/crime-info.model.ts
+++ src/app/database/crime-info.model.ts
@@ -1,3 +1,25 @@
-export interface crimeInfo {
-    
+class Point {
+  latitude: number;
+  longitude: number;
+  human_address: string;
+}
+
+export interface CrimeInfo {
+  $recordid: number;
+  callnumber: string;
+  calldatetime: string;
+  priority: string
+  district: string;
+  description: string;
+  incidentlocation: string;
+  zipcode: string;
+  neighborhood: string;
+  policedistrict: string;
+  policepost: string;
+  councildistrict: string;
+  sheriffdistricts: string;
+  community_statistical_areas: string;
+  census_tracts: string;
+  vrizones: string;
+  location: Point;
 }
--- src/app/database/database.service.ts
+++ src/app/database/database.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/database';
-import { Observable } from 'rxjs';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';
 
 @Injectable({
   providedIn: 'root'
 })
+
+// https://www.positronx.io/create-angular-7-firebase-crud-app-with-angular-material-7/
+// https://www.positronx.io/angular-7-firebase-5-crud-operations-with-reactive-forms/
 export class DatabaseService {
+  callsRef: AngularFireList<any>;
+  callRef: AngularFireObject<any>;
+
   constructor(private db: AngularFireDatabase) { }
 
-  private _get
+  /* Get a call */
+  GetCall(id: string) {
+    this.callRef = this.db.object('calls/' + id);
+    return this.callRef;
+  }
+
+  /* Get list of calls */
+  GetCalls() {
+    this.callsRef = this.db.list('calls');
+    return this.callsRef;
+  }
 }
