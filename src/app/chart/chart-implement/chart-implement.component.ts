8c91d0b47209a20d9f80181b7085d4458a3573d4
Corey Scheideman <coreys1@umbc.edu>
2020-05-12 15:23:38 -0400

    implement call count by district bar chart

--- src/app/chart/chart-implement/chart-implement.component.ts
+++ src/app/chart/chart-implement/chart-implement.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { DatabaseService } from './../../database/database.service';
-import { CrimeInfo } from './../../database/crime-info.model';
+import { CrimeInfo, DistrictCounts } from './../../database/crime-info.model';
+// https://www.amcharts.com/docs/v4/getting-started/using-typescript-or-es6/#Module_list
+import * as am4core from '@amcharts/amcharts4/core';
+import * as am4charts from '@amcharts/amcharts4/charts';
+import am4themes_animated from '@amcharts/amcharts4/themes/animated';
+
+// Themes begin
+am4core.useTheme(am4themes_animated);
+// Themes end
 
 @Component({
   selector: 'app-chart-implement',
@@ -9,23 +17,48 @@ import { CrimeInfo } from './../../database/crime-info.model';
 })
 export class ChartImplementComponent implements OnInit {
   // https://www.positronx.io/create-angular-7-firebase-crud-app-with-angular-material-7/
-  callData: any = [];
+  callData: CrimeInfo[];
+  data: DistrictCounts[];
 
   constructor(private dbApi: DatabaseService) {
-    this.dbApi.GetCalls()
-    .snapshotChanges()
-    .subscribe(calls => {
-      calls.forEach(item => {
-        let a = item.payload.toJSON();
-        a['$KEY'] = item.key;
-        this.callData.push(a as CrimeInfo)
-      })
-      console.log(this.callData);
-    })
+    this.callData = this.dbApi.GetCalls()
+    this.data = this.dbApi.GetNumCallsByDistrict();
   }
 
-  // do whatever with callData
+  ngOnInit(): void { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {}
+
+  onDisplayCallsByDistrictBarChart():void {
+    console.log('onDisplay()');
+    /* Chart code */
+    // Create chart instance
+    let chart = am4core.create("num-calls-by-district-barchart", am4charts.XYChart);
+
+    // add and sort data
+    chart.data = this.data;
+    chart.data.sort((a, b) => (a.count > b.count ? -1 : 1));
+
+    // Create axes
+    let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.dataFields.category = "district";
+    categoryAxis.renderer.grid.template.location = 0;
+    categoryAxis.renderer.minGridDistance = 30;
+    categoryAxis.title.text = "District";
+
+    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.title.text = "# Calls";
+
+    // Create series
+    let series = chart.series.push(new am4charts.ColumnSeries());
+    series.dataFields.valueY = "count";
+    series.dataFields.categoryX = "district";
+    series.name = "Calls By District";
+    series.columns.template.tooltipText = "{categoryX}: [bold]{valueY}[/]";
+    series.columns.template.fillOpacity = .8;
+
+    let columnTemplate = series.columns.template;
+    columnTemplate.strokeWidth = 2;
+    columnTemplate.strokeOpacity = 1;
   }
 }
