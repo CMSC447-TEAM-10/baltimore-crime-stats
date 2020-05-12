2344347a4ebaf75f2e3a9bdea81e88b160b72b60
Corey Scheideman <coreys1@umbc.edu>
2020-05-11 21:18:08 -0400

    get data from Firebase

--- src/app/chart/chart-implement/chart-implement.component.ts
+++ src/app/chart/chart-implement/chart-implement.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from './../../database/database.service';
+import { CrimeInfo } from './../../database/crime-info.model';
 
 @Component({
   selector: 'app-chart-implement',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chart-implement.component.css']
 })
 export class ChartImplementComponent implements OnInit {
+  // https://www.positronx.io/create-angular-7-firebase-crud-app-with-angular-material-7/
+  callData: any = [];
 
-  constructor() { }
+  constructor(private dbApi: DatabaseService) {
+    this.dbApi.GetCalls()
+    .snapshotChanges()
+    .subscribe(calls => {
+      calls.forEach(item => {
+        let a = item.payload.toJSON();
+        a['$KEY'] = item.key;
+        this.callData.push(a as CrimeInfo)
+      })
+      console.log(this.callData);
+    })
+  }
+
+  // do whatever with callData
 
   ngOnInit(): void {
   }
-
 }
