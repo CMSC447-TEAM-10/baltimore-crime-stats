f2835032864a8fd58ea7fb3e76ed89bd63c15376
Juno Park <GothLoli@Junos-MBP.fios-router.home>
2020-05-22 08:24:45 -0400

    fixed all

--- src/app/database/database.service.spec.ts
+++ src/app/database/database.service.spec.ts
@@ -2,17 +2,22 @@ import { TestBed } from '@angular/core/testing';
 
 import { DatabaseService } from './database.service';
 
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
 describe('DatabaseService', () => {
   let service: DatabaseService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ AngularFireModule.initializeApp(environment.firebase),
+        AngularFireDatabaseModule ]
+    });
     service = TestBed.inject(DatabaseService);
   });
 
-  /*
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-  */
 });
