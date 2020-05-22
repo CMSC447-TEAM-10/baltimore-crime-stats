bab438c3e40431c38d74d2c58738327d492af60f
Juno Park <GothLoli@Junos-MBP.fios-router.home>
2020-05-21 20:40:35 -0400

    fixed testing logic

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -26,10 +26,13 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('baltimore-crime-stats');
   });
 
+  // we don't want to render title via AppComponent
+
+  /*
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('baltimore-crime-stats app is running!');
-  });
+  });*/
 });
