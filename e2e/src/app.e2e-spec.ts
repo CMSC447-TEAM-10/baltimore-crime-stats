60ab027a6bdb0fbcb44371feb3f6c2cd0aa1bfb1
Azaria Gallo <agallo4@umbc.edu>
2020-05-22 14:39:57 -0400

    Modifying files for e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,21 +1,64 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, element, by } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
+    browser.get('/');
   });
 
-  // We are using NavBar to display title
-  /*
-  it('should display welcome message', () => {
+  it('should check baltimore city data button', () => {
+    element(by.buttonText('Baltimore City Data')).click();
+  });
+
+  it('should check each button on map page', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('baltimore-crime-stats app is running!');
+    element(by.name('map')).click();
+    
+    /*element(by.name('low')).click();
+    element(by.name('medium')).click();
+    element(by.name('high')).click();
+    element(by.name('all')).click();
+    */
   });
-  */
 
+  it('should check each button on charts page', () => {
+    page.navigateTo();
+    element(by.name('charts')).click();
+    element(by.buttonText('Show Calls by District')).click();
+    //element(by.buttonText('Show Calls over Time')).click();
+  });
+
+  it('should check each button on pie charts page', () => {
+    page.navigateTo();
+    element(by.name('pie charts')).click();
+    element(by.buttonText('Show Chart')).click();
+   /*
+    element(by.buttonText('Low')).click();
+    element(by.buttonText('Medium')).click();
+    element(by.buttonText('High')).click();
+    element(by.buttonText('All')).click();
+    */
+    //element(by.buttonText('Baltimore City Data')).click();
+  });
+
+  it('should check each button on directed tree page', () => {
+    page.navigateTo();
+    element(by.name('directed tree')).click();
+    element(by.buttonText('Show Tree')).click();
+   /*
+    element(by.buttonText('Low')).click();
+    element(by.buttonText('Medium')).click();
+    element(by.buttonText('High')).click();
+    element(by.buttonText('All')).click();
+    */
+    //element(by.buttonText('Baltimore City Data')).click();
+  });
+
+
+  /*
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
@@ -23,4 +66,6 @@ describe('workspace-project App', () => {
       level: logging.Level.SEVERE,
     } as logging.Entry));
   });
+*/
+
 });
