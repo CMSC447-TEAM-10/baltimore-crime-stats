ce9035379967aa1483f48a2de7a29a35bb85d957
Juno Park <GothLoli@Junos-MBP.fios-router.home>
2020-05-22 06:29:05 -0400

    added error handler

--- src/app/database/crime-info.model.ts
+++ src/app/database/crime-info.model.ts
@@ -1,40 +1,40 @@
 class Point {
-  latitude: number;
-  longitude: number;
-  human_address: string;
+  latitude?: number;
+  longitude?: number;
+  human_address?: string;
 }
 
 export interface CrimeInfo {
-  $recordid: number;
-  callnumber: string;
-  calldatetime: string;
-  priority: string
-  district: string;
-  description: string;
-  incidentlocation: string;
-  zipcode: string;
-  neighborhood: string;
-  policedistrict: string;
-  policepost: string;
-  councildistrict: string;
-  sheriffdistricts: string;
-  community_statistical_areas: string;
-  census_tracts: string;
-  vrizones: string;
-  location: Point;
+  $recordid?: number;
+  callnumber?: string;
+  calldatetime?: string;
+  priority?: string
+  district?: string;
+  description?: string;
+  incidentlocation?: string;
+  zipcode?: string;
+  neighborhood?: string;
+  policedistrict?: string;
+  policepost?: string;
+  councildistrict?: string;
+  sheriffdistricts?: string;
+  community_statistical_areas?: string;
+  census_tracts?: string;
+  vrizones?: string;
+  location?: Point;
 }
 
 export interface DistrictCounts {
-  district: string;
-  count: number;
+  district?: string;
+  count?: number;
 }
 
 export interface TimeCount {
-    calldatetime: Date;
-    count: number;
+    calldatetime?: Date;
+    count?: number;
 }
 
 export interface CrimeType {
-    desc: string;
-    count: number;
+    desc?: string;
+    count?: number;
 }
